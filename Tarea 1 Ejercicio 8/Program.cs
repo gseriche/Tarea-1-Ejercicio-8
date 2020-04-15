87c4413fe3ed9886a3a7694e58e996274f156afa
Gonzalo Seriche Vega <gonzalo.seriche@gmail.com>
2020-04-15 18:35:25 -0400

    primer commit para Ejercicio 8

---
+++ Tarea 1 Ejercicio 8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1_Ejercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 8) Diseñar un algoritmo y construir un programa que permita ingresar una cadena de caracteres por teclado y la imprima al revés.
+
+            Console.WriteLine("Ingrese la palabra a reversar: ");
+
+            string frase = Console.ReadLine();
+            List<char> letras = new List<char>();
+            foreach (char letra in frase)
+            {
+                //Console.WriteLine(letra);
+                letras.Add(letra);
+            }
+            letras.Reverse();
+            foreach (char letra in letras)
+            {
+                Console.WriteLine(letra);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
